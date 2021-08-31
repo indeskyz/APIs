7ecc6893e0b772718b6cb59b96924651fa7da3d2
Tanner <Tanner@Email.ca>
2021-08-31 05:22:40 -0400

    added IOS app

---
+++ PublicApps/MobileApps/IOS/TodoList/Models/TodoItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  TodoItem+CoreDataClass.swift
+//  TodoList
+//
+//  Created by hazel on 2021-08-30.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(TodoItem)
+public class TodoItem: NSManagedObject {
+
+}
---
+++ PublicApps/MobileApps/IOS/TodoList/Views/StackView.swift
@@ -0,0 +1,8 @@
+//
+//  StackView.swift
+//  TodoList
+//
+//  Created by hazel on 2021-08-30.
+//
+
+import Foundation
